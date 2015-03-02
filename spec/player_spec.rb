a509cb1fd44599335447a4611972eaff09d42a0a
Alabê Duarte <alabeduarte@gmail.com>
2015-03-02 20:33:04 -0300

    Add rspec

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Player do
+
+  context 'You see before yourself a long hallway with stairs at the end. There is nothing in the way.' do
+    xit 'walks forward' do
+
+    end
+  end
+end
