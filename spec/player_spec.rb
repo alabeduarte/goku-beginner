8e9630bdffcbb9e2f1c247193de17e62d9ad9fbd
Alabê Duarte <alabeduarte@gmail.com>
2015-03-02 20:57:52 -0300

    [Test Failing] - Create test for warrior walk foward

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Player do
 
   context 'You see before yourself a long hallway with stairs at the end. There is nothing in the way.' do
-    xit 'walks forward' do
+    it 'walks forward' do
+      warrior = double(:walk!)
 
+      expect(warrior).to receive(:walk!)
+
+      subject.play_turn warrior
     end
   end
 end
