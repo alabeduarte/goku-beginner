e520843025f53c7899a13bf8b71e5a761a77542d
Alabê Duarte <aduarte@thoughtworks.com>
2015-03-02 21:31:32 -0300

    [Test passing] The Warrior attacks when there is anything ahead otherwise he walks

--- player.rb
+++ player.rb
@@ -1,5 +1,9 @@
 class Player
   def play_turn(warrior)
-    warrior.walk! if warrior.feel.empty?
+    unless warrior.feel.empty?
+      warrior.attack!
+    else
+      warrior.walk!
+    end
   end
 end
